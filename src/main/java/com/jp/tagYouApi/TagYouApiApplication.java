3b02b0a0b3842d33093dfc74a6cd6804d3cbc005
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2024-12-14 23:18:03 -0300

    first commit

---
+++ src/main/java/com/jp/tagYouApi/TagYouApiApplication.java
@@ -0,0 +1,13 @@
+package com.jp.tagYouApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagYouApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagYouApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/tagYouApi/TagYouApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.tagYouApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagYouApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
